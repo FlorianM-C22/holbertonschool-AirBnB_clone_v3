9d792442d43331f724c277ff2199dace959dacf1
FlorianM-C22 <7484@holbertonstudents.com>
2024-04-17 12:00:12 +0200

    Task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-""""Index module for the API"""
-
+"""Start your API"""
 from flask import jsonify
-from models import storage
 from api.v1.views import app_views
+from models import storage
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
@@ -12,21 +11,21 @@ from models.state import State
 from models.user import User
 
 
-@app_views.route('/status', strict_slashes=False)
-def get_status():
-    """Return the status of the API"""
+@app_views.route("/status", strict_slashes=False)
+def display_status():
+    """returns a JSON"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False)
-def count_objs():
+@app_views.route("/stats", strict_slashes=False)
+def numbers_of_objs():
     """Retrieves the number of each objects by type"""
-    stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    }
-    return jsonify(stats)
+    obj_list = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+        }
+    return jsonify(obj_list)
