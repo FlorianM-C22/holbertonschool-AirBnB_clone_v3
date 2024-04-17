9d540c3ec39298738c39376adc752b07f9576f54
FlorianM-C22 <7484@holbertonstudents.com>
2024-04-17 16:24:22 +0200

    Task 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,24 +5,26 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    """Handling 404 error"""
-    return jsonify(error="Not found"), 404
-
-
 @app.teardown_appcontext
-def teardown(self):
-    """Closes the storage"""
+def teardown(exception):
+    """closes the storage on teardown"""
     storage.close()
 
 
-if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.environ.get("HBNB_API_PORT", 5000))
+@app.errorhandler(404)
+def page_not_found(exception):
+    """404 error"""
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == '__main__':
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.environ.get('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
